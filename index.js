10ead8d66a907290dbb310994455dfaa59e7bb32
Bob <bob_w@qq.com>
2021-02-16 23:04:55 +0800

    安装file-loader

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import a from "./a";
 import b from "./b";
 
+const icon = require('./icon.jpg');
+
 a();
 b();
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    // 绝对路径
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  }
+}
