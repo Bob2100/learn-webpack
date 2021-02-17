a1ad3837e5661df3bd6f7d763c45fd76af49b04a
Bob <bob_w@qq.com>
2021-02-17 19:29:04 +0800

    babel polyfill注入es6新特性

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './index2.js',
+  entry: ["@babel/polyfill", './index2.js'],
   output: {
     filename: 'bundle.js',
     // 绝对路径
